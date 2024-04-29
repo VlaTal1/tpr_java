f5edea47ae2078de7aeb5130b811d2dc2f4d6bc5
VladyslavTalakh <vladiktalah@gmail.com>
2024-04-29 09:07:03 +0300

    Fixed tests

--- src/test/java/org/example/printer/VehiclePrinterTest.java
+++ src/test/java/org/example/printer/VehiclePrinterTest.java
@@ -1,7 +1,7 @@
 package org.example.printer;
 
 import org.example.bom.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
