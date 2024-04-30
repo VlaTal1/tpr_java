0e5cb3994bec7ed01495e7c35be40e73d573e507
VladyslavTalakh <vladiktalah@gmail.com>
2024-04-30 17:21:11 +0300

    Added ClientRepository

--- src/main/java/org/example/repository/ClientRepository.java
+++ src/main/java/org/example/repository/ClientRepository.java
@@ -1,7 +1,6 @@
 package org.example.repository;
 
-import org.example.bom.Client;
-import org.example.dto.ClientDTO;
+import org.example.dto.db.ClientDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
