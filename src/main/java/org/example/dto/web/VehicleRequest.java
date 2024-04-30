f3df4f6188f44c0287434594f08832310420a743
VladyslavTalakh <vladiktalah@gmail.com>
2024-04-30 17:22:16 +0300

    Added VehicleRequest and its converter with factory usage

---
+++ src/main/java/org/example/dto/web/VehicleRequest.java
@@ -0,0 +1,32 @@
+package org.example.dto.web;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.bom.Color;
+import org.example.bom.Model;
+import org.example.bom.Type;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleRequest {
+
+    @JsonProperty(required = false)
+    private Long id;
+
+    private int amount;
+
+    private Model model;
+
+    private Color color;
+
+    private double price;
+
+    private int year;
+
+    private Type type;
+}
