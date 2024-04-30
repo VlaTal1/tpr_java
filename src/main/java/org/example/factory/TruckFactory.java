6e0cee9a2bbe00819d5dc199c65a3990428edd2a
VladyslavTalakh <vladiktalah@gmail.com>
2024-04-30 17:20:58 +0300

    Added VehicleFactory and appropriate factory realizations.

---
+++ src/main/java/org/example/factory/TruckFactory.java
@@ -0,0 +1,16 @@
+package org.example.factory;
+
+import org.example.bom.Color;
+import org.example.bom.Model;
+import org.example.bom.Truck;
+import org.example.bom.Vehicle;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TruckFactory implements VehicleFactory {
+
+    @Override
+    public Vehicle createVehicle(Long id, int amount, Model model, Color color, double price, int year) {
+        return new Truck(id, amount, model, color, price, year);
+    }
+}
---
+++ src/main/java/org/example/factory/VehicleFactory.java
@@ -0,0 +1,10 @@
+package org.example.factory;
+
+import org.example.bom.Color;
+import org.example.bom.Model;
+import org.example.bom.Vehicle;
+
+public interface VehicleFactory {
+
+    Vehicle createVehicle(Long id, int amount, Model model, Color color, double price, int year);
+}
