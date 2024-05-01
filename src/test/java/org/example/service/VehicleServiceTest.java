da35bf71e8c174a937bed1e712c38465488438e6
VladyslavTalakh <vladiktalah@gmail.com>
2024-05-01 16:52:21 +0300

    Fixed VehicleServiceTest

--- src/test/java/org/example/service/VehicleServiceTest.java
+++ src/test/java/org/example/service/VehicleServiceTest.java
@@ -1,7 +1,19 @@
 package org.example.service;
 
 import org.example.bom.*;
+import org.example.converter.Converter;
+import org.example.dto.db.VehicleDTO;
+import org.example.repository.VehicleRepository;
+import org.example.service.ColorService.ColorService;
+import org.example.service.CountryService.CountryService;
+import org.example.service.ManufacturerService.ManufacturerService;
+import org.example.service.ModelService.ModelService;
+import org.example.service.VehicleService.VehicleServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VehicleServiceTest {
@@ -14,9 +26,34 @@ public class VehicleServiceTest {
 
     private final Color color = new Color(1L, "Red");
 
+    @Mock
+    private Converter<VehicleDTO, Vehicle> vehicleConverter;
+
+    @Mock
+    private VehicleRepository vehicleRepository;
+
+    @Mock
+    private ColorService colorService;
+
+    @Mock
+    private ModelService modelService;
+
+    @Mock
+    private ManufacturerService manufacturerService;
+
+    @Mock
+    private CountryService countryService;
+
+    private VehicleServiceImpl vehicleService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        vehicleService = new VehicleServiceImpl(vehicleConverter, vehicleRepository, colorService, modelService, manufacturerService, countryService);
+    }
+
     @Test
     public void testPrintVehiclePassengerCar() {
-        VehicleService service = new VehicleService();
         Vehicle vehicle = new PassengerCar(1L, 5000, model, color, 6000, 2022);
         String expectedOutput = "Vehicle ID: 1\n" +
                 "Amount: 5000\n" +
@@ -26,12 +63,11 @@ public class VehicleServiceTest {
                 "Price: 6000.0\n" +
                 "Year: 2022\n" +
                 "Credit: 91\n";
-        assertEquals(expectedOutput, service.printVehicle(vehicle));
+        assertEquals(expectedOutput, vehicleService.printVehicle(vehicle));
     }
 
     @Test
     public void testPrintVehicleTruck() {
-        VehicleService service = new VehicleService();
         Vehicle vehicle = new Truck(1L, 5000, model, color, 6000, 2022);
         String expectedOutput = "Vehicle ID: 1\n" +
                 "Amount: 5000\n" +
@@ -41,12 +77,11 @@ public class VehicleServiceTest {
                 "Price: 6000.0\n" +
                 "Year: 2022\n" +
                 "Credit: 15\n";
-        assertEquals(expectedOutput, service.printVehicle(vehicle));
+        assertEquals(expectedOutput, vehicleService.printVehicle(vehicle));
     }
 
     @Test
     public void testPrintVehicleMotorcycle() {
-        VehicleService service = new VehicleService();
         Vehicle vehicle = new Motorcycle(1L, 5000, model, color, 6000, 2022);
         String expectedOutput = "Vehicle ID: 1\n" +
                 "Amount: 5000\n" +
@@ -56,12 +91,11 @@ public class VehicleServiceTest {
                 "Price: 6000.0\n" +
                 "Year: 2022\n" +
                 "Credit: 188\n";
-        assertEquals(expectedOutput, service.printVehicle(vehicle));
+        assertEquals(expectedOutput, vehicleService.printVehicle(vehicle));
     }
 
     @Test
     public void testPrintVehicleNotFound() {
-        VehicleService service = new VehicleService();
         Vehicle vehicle = new Vehicle(1L, 5000, model, color, 6000, 2022) {
             @Override
             public int calculateCredit() {
@@ -69,7 +103,7 @@ public class VehicleServiceTest {
             }
         };
         String expectedOutput = "No printer found";
-        assertEquals(expectedOutput, service.printVehicle(vehicle));
+        assertEquals(expectedOutput, vehicleService.printVehicle(vehicle));
     }
 }
 
