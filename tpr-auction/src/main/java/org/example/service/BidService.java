0951ff2f6d251c56037be85f79793f4e9abe6c56
VladyslavTalakh <vladiktalah@gmail.com>
2024-05-20 21:27:06 +0300

    Saving bid history

--- tpr-auction/src/main/java/org/example/service/BidService.java
+++ tpr-auction/src/main/java/org/example/service/BidService.java
@@ -3,16 +3,24 @@ package org.example.service;
 import lombok.RequiredArgsConstructor;
 import org.example.bom.Auction;
 import org.example.bom.AuctionStatus;
+import org.example.bom.BidHistory;
 import org.example.bom.Client;
 import org.example.converter.Converter;
+import org.example.dto.db.AuctionDTO;
 import org.example.dto.db.BidDTO;
+import org.example.dto.db.BidHistoryDTO;
 import org.example.dto.db.ClientDTO;
 import org.example.dto.web.request.BidRequest;
 import org.example.exception.AuctionEndedException;
 import org.example.exception.AuctionNotFoundException;
 import org.example.exception.ClientNotFoundException;
+import org.example.repository.BidHistoryRepository;
 import org.example.repository.BidRepository;
 import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -20,11 +28,15 @@ public class BidService {
 
     private final BidRepository bidRepository;
 
+    private final BidHistoryRepository bidHistoryRepository;
+
     private final AuctionService auctionService;
 
     private final ClientService clientService;
 
     private final Converter<ClientDTO, Client> clientConverter;
+
+    private final Converter<AuctionDTO, Auction> auctionConverter;
 
     public void placeBid(Long auctionId, BidRequest bidRequest) throws AuctionNotFoundException, AuctionEndedException, ClientNotFoundException {
         Auction auction = auctionService.findById(auctionId);
@@ -35,6 +47,12 @@ public class BidService {
                     .amount(bidRequest.getAmount())
                     .build();
             bidRepository.save(bidDTO);
+            BidHistoryDTO bidHistoryDTO = BidHistoryDTO.builder()
+                    .bid(bidDTO)
+                    .auction(auctionConverter.toDTO(auction))
+                    .time(Timestamp.valueOf(LocalDateTime.now()))
+                    .build();
+            bidHistoryRepository.save(bidHistoryDTO);
             auctionService.resetAuctionTimer(auctionId);
         } else {
             throw new RuntimeException("Auction has ended");
